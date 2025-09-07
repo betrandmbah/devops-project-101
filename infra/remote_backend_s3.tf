9d97c9f542dbfb0837d7eb88f0507845bb8c60d2
betrand <tekeh.betrand@gmail.com>
2025-09-07 18:24:37 -0400

    nn

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-minis"
+    bucket = "dev-proj-1-remote-state-bucket-miniss"
     key    = "devops-project-1/terraform.tfstate"
     region = "us-west-1"
   }
