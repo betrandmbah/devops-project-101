c1dc0220d16f312b1e971a7f2c0d63fe6ff94494
betrand <tekeh.betrand@gmail.com>
2025-09-07 17:55:07 -0400

    new

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region                   = "us-west-1"
-  shared_credentials_files = ["/c/Users/mbah_/.aws/credentials"]
+  shared_credentials_files = ["/Users/mbah_/.aws/credentials"]
 }
